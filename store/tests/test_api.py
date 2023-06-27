6739aa9e6ce2f799b5128d9b0e3813054e398c49
Lord Divan <darveter667@gmail.com>
2023-06-27 14:45:27 +0300

    tests were added, little refactoring

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,3 +1,7 @@
+import json
+from decimal import Decimal
+
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -9,6 +13,7 @@ from store.serializers import BookSerializer
 class BooksApiTestCase(APITestCase):
 
     def setUp(self):
+        self.test_user = User.objects.create(username="test_username", password="test_password")
         self.book1 = Book.objects.create(name='Testbook1 John', price='111.34')
         self.book2 = Book.objects.create(name='Testbook2', price='111.00')
         self.book3 = Book.objects.create(name='Testbook3', price='0.23',
@@ -23,8 +28,6 @@ class BooksApiTestCase(APITestCase):
             [self.book1, self.book2,
              self.book3, self.book4],
             many=True).data
-        if response.status_code == 403:
-            return
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
@@ -33,8 +36,6 @@ class BooksApiTestCase(APITestCase):
         serializer_data = BookSerializer([
             self.book1, self.book4
         ], many=True).data
-        if response.status_code == 403:
-            return
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
@@ -42,8 +43,6 @@ class BooksApiTestCase(APITestCase):
         response = self.client.get(self.url, data={'price': '0.23'})
         serializer_data = BookSerializer([self.book3],
                                          many=True).data
-        if response.status_code == 403:
-            return
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
@@ -53,7 +52,60 @@ class BooksApiTestCase(APITestCase):
             self.book3, self.book2,
             self.book1, self.book4
         ], many=True).data
-        if response.status_code == 403:
-            return
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_create(self):
+        self.assertEqual(4, Book.objects.all().count())
+        data = {
+            "name": "Scooby Do 2",
+            "price": "123.12",
+            "author": "Shaggy"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.test_user)
+        response = self.client.post(self.url,
+                                    data=json_data,
+                                    content_type="application/json")
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(5, Book.objects.all().count())
+
+    def test_update(self):
+        url = reverse_lazy('book-detail', args=(self.book1.id,))
+        self.assertEqual(4, Book.objects.all().count())
+        data = {
+            "name": self.book1.name,
+            "price": 990000,
+            "author": self.book1.author
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.test_user)
+        response = self.client.put(url,
+                                   data=json_data,
+                                   content_type="application/json")
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+        self.book1.refresh_from_db()
+        self.assertEqual(Decimal(990000), self.book1.price)
+
+    def test_delete(self):
+        url = reverse_lazy('book-detail', args=(self.book1.id,))
+        self.assertEqual(4, Book.objects.all().count())
+        self.client.force_login(self.test_user)
+        response = self.client.delete(url)
+        self.assertEqual(204, response.status_code)
+        self.assertEqual(3, Book.objects.all().count())
+
+    def test_get_detail(self):
+        url = reverse_lazy('book-detail', args=(self.book1.id,))
+        json_data = {
+            "id": self.book1.id,
+            "name": self.book1.name,
+            "price": self.book1.price,
+            "author": self.book1.author
+        }
+        response = self.client.get(url)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(json_data, response.data)
